a5d0ecee11bca816242ca10b330f0095db55488a
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-11-14 16:05:10 +0300

    add class for fetch position user

--- RememberTheMan/LocationFetch.swift
+++ RememberTheMan/LocationFetch.swift
@@ -5,4 +5,23 @@
 //  Created by Marat Fakhrizhanov on 14.11.2024.
 //
 
-import Foundation
+import CoreLocation
+
+class LocationFetcher: NSObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    var lastKnownLocation: CLLocationCoordinate2D?
+
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+
+    func start() {
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        lastKnownLocation = locations.first?.coordinate
+    }
+}
