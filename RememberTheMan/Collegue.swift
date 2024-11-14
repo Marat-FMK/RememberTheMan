087e0c402161340449188135e9ce9cfb24a3c28d
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-11-14 16:02:53 +0300

    fix model

--- RememberTheMan/Collegue.swift
+++ RememberTheMan/Collegue.swift
@@ -14,11 +14,15 @@ class Collegue {
     var id = UUID()
     var name: String
     var phone: String
+    var longitude: Double?
+    var latitude: Double?
     @Attribute(.externalStorage) var photo: Data
     
-    init(name: String, phone: String, photo: Data) {
+    init(name: String, phone: String, photo: Data, longitude: Double?, latitude: Double?) {
         self.name = name
         self.phone = phone
         self.photo = photo
+        self.longitude = longitude
+        self.latitude = latitude
     }
 }
