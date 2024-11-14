e7a49b84779629d77a2a39f4c24bc175ac100846
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-11-14 16:05:31 +0300

    add position map view

--- RememberTheMan/add colleague View.swift
+++ RememberTheMan/add colleague View.swift
@@ -5,6 +5,9 @@
 //  Created by Marat Fakhrizhanov on 13.11.2024.
 //
 
+
+import MapKit
+import CoreLocation
 import SwiftData
 import SwiftUI
 
@@ -12,40 +15,59 @@ import SwiftUI
 struct AddNewСolleague: View {
     @Environment(\.modelContext) var modelContext
     
+
     @State private var name = ""
     @State private var phoneNumber = ""
     
     @Binding var photo: Data?
     
+    let coordinates: CLLocationCoordinate2D?
+    
     var body : some View {
         NavigationStack {
-            VStack(spacing: 44 ) {
-                
-                Image(uiImage: UIImage(data: photo!)!)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width:300)
-                
-                TextField(" name ", text: $name)
-                    .textFieldStyle(.roundedBorder)
-                
-                TextField("phone number", text: $phoneNumber )
-                    .textFieldStyle(.roundedBorder)
-                
-                Button("Save"){
-                    let newCollegue = Collegue(name: name, phone: phoneNumber, photo: photo!)
-                    modelContext.insert(newCollegue)
+            ScrollView {
+                VStack(spacing: 44 ) {
+                    
+                    Image(uiImage: UIImage(data: photo!)!)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width:300)
+                    
+                    TextField(" name ", text: $name)
+                        .textFieldStyle(.roundedBorder)
+                    
+                    TextField("phone number", text: $phoneNumber )
+                        .textFieldStyle(.roundedBorder)
+                    
+                    Button("Save"){
+                        
+                        let newCollegue = Collegue(
+                            name: name,
+                            phone: phoneNumber,
+                            photo: photo!,
+                            longitude: Double(coordinates?.longitude.description ?? "1"),
+                            latitude: Double( coordinates?.latitude.description ?? "1" ))
+                        
+                        modelContext.insert(newCollegue)
+                    }
+                    
+                    NavigationLink("see All") {
+                        ListOfCollegue()
+                    }
+                    
+                    NavigationLink("add another") {
+                        ContentView()
+                    }
+                    
+                    Map(){
+                        Marker("Collegue", coordinate: coordinates!)
+                    }
+                    .mapStyle(.hybrid)
+                    .frame(width:360, height: 360)
+                    
                 }
-                
-                NavigationLink("see All") {
-                    ListOfCollegue()
-                }
-                
-                NavigationLink("add another") {
-                    ContentView()
-                }
+                .padding()
             }
-            .padding()
         }
     }
 }
